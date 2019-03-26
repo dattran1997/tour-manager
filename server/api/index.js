c216b0517e4d64282515ed076601541e1c6bbf87
dattran <dattran1997@gmail.com>
2019-03-26 14:57:35 +0700

    dat-backend-ver2

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const tourRouter = require('./tour/routes');
+const userRouter = require('./user/routes');
+const jobRouter = require('./job/routes');
+const authRouter = require('./auth/routes');
 
 const apiRouter = express.Router();
 
 apiRouter.use('/api/tour',tourRouter);
+apiRouter.use('/api/user',userRouter);
+apiRouter.use('./api/job',jobRouter);
+apiRouter.use('./api/auth',authRouter);
 
 
 module.exports = apiRouter;
