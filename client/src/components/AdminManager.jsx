c6d45fa8460ead6a1fc3c9e02c78c6b2180cfc67
dattran <dattran1997@gmail.com>
2019-04-08 23:50:29 +0700

    'delele'

---
+++ client/src/components/AdminManager.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AdminManager extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const apiRouter = require('./api/index');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
 
 const tourServer = async function(){
     try{
@@ -12,10 +14,19 @@ const tourServer = async function(){
         await mongoose.connect('mongodb://localhost:27017/tour-manager');
 
         //user middleware + routers
+        app.use(cors({
+            origin: ['http://localhost:9999','http://localhost:3000'],
+            credentials: true,
+        }))
         app.use(bodyParser.urlencoded({
             extended: false
         }));
         app.use(bodyParser.json());
+        app.use(session({
+            secret: 'keyboard cat',
+            resave: false,
+            saveUninitialized: true,
+        }));
         app.use(apiRouter);
         
         // start server
