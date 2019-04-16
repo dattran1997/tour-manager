96bbfab065a8a3ea36237ad9e30932bd306137ed
kirr <rongnhat2@gmail.com>
2019-04-16 12:11:42 +0700

    tour-conmit-3

--- server/api/tour/routes.js
+++ server/api/tour/routes.js
@@ -61,5 +61,21 @@ tourRoute.post('/edit/:id', async function(req,res){
 });
 
 
+tourRoute.post('/delete/:id', async function(req,res){
+    console.log(req.params.id);
+    const id = req.params.id;
+
+    try{
+        const deleteTour = await TourModel.findByIdAndDelete({_id: id});
+        res.json({
+            success:true
+        });
+    }catch(error){
+        res.json({
+            message:error
+        });
+    }
+});
+
 module.exports = tourRoute;
 
